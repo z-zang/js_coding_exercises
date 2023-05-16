106e2808f9dacbff7c38b38097c69f858f7a436a
Zichao Zang <zang.code@gmail.com>
2023-05-16 20:17:44 +0100

    Update and refine solutions for ex 3
    Solutions slightly re-written for better readability (less one-liners)

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,43 +1,43 @@
-import { capitalize } from "./exercise001";
+import { capitalize } from "./exercise001"; // Hope this doesn't count as cheating!
 
 export function getSquares(nums) {
 	if (nums === undefined) throw new Error('nums is required');
 	// Your code here!
-	if (nums.length === 0) return []
+	if (nums.length === 0) return [];
+	const squares = nums.map((num) => num ** 2);
 
-	const squares = nums.map((num) => num ** 2 )
-	return squares
+	return squares;
 }
 
 export function camelCaseWords(words) {
 	if (words === undefined) throw new Error('words is required');
 	// Your code here!
 	const camelCasedArray = words.map((word, index) => {
-		if (index === 0) return word
-		else {
-			return capitalize(word)
-		}
+		const isFirstWord = index === 0;
+		return isFirstWord ? word : capitalize(word);
 	})
-	return camelCasedArray.join("")
+	return camelCasedArray.join("");
 }
 
 export function getTotalSubjects(people) {
 	if (people === undefined) throw new Error('people is required');
 	// Your code here!
-	let subjects = 0
+
+	let totalSubjects = 0;
 	people.forEach((person) => {
-		subjects += person.subjects.length
+		totalSubjects += person.subjects.length;
 	})
-	return subjects
+	return totalSubjects;
 }
 
 export function checkIngredients(menu, ingredient) {
 	if (menu === undefined) throw new Error('menu is required');
 	if (!ingredient) throw new Error('ingredient is required');
 	// Your code here!
-	return menu.some(menuItem => {
-		return menuItem.ingredients.indexOf(ingredient) !== -1
+	const doesMenuContainIngredients = menu.some(menuItem => {
+		return menuItem.ingredients.indexOf(ingredient) !== -1;
 	})
+	return doesMenuContainIngredients;
 }
 
 export function duplicateNumbers(arr1, arr2) {
@@ -48,7 +48,13 @@ export function duplicateNumbers(arr1, arr2) {
 	const duplicates = [];
 
 	arr1.forEach(number => {
-		arr2.indexOf(number) !== -1 && duplicates.indexOf(number) === -1 && duplicates.push(number)
+		const isInArr2 = arr2.indexOf(number) !== -1;
+		const isInDuplicatesArr = duplicates.indexOf(number) === -1;
+
+		isInArr2 && isInDuplicatesArr && duplicates.push(number);
 	})
-	return duplicates.sort()
+
+	const sortedDuplicates = duplicates.sort();
+
+	return sortedDuplicates;
 }
