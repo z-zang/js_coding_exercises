4f4833a7c502a074eedfc0be09eeddc9589a7e98
Zichao Zang <zang.code@gmail.com>
2023-05-16 20:17:22 +0100

    Update and refine solutions for ex 2
    Solutions slightly re-written for better readability (less one-liners)

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,30 +1,33 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
 	// Your code here!
-	return sandwich.fillings
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
 	// Your code here!
-	return person.city === 'Manchester'
+	return person.city === 'Manchester';
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
 	// Your code here!
-	return Math.ceil(people/40)
+	return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
 	// Your code here!
-	const sheepArray = arr.filter((animal) => animal === 'sheep')
-	return sheepArray.length
+	const sheepArray = arr.filter((animal) => animal === 'sheep');
+	return sheepArray.length;
 }
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
 	// Your code here!
-	return person?.address?.postCode.charAt(0) === 'M' && person?.address?.city === 'Manchester'
+	const isMPostcode = person?.address?.postCode.charAt(0) === 'M';
+	const isCityManchester = person?.address?.city === 'Manchester';
+
+	return isMPostcode && isCityManchester;
 }
